94a275922a7e8d1567da604983e67a45f24b9651
MrPink <rubberandrobber@gmail.com>
2022-01-27 15:16:33 +0300

    active ragdoll

--- PartyFpsTactics/Assets/Scripts/HumanVisualController.cs
+++ PartyFpsTactics/Assets/Scripts/HumanVisualController.cs
@@ -1,11 +1,25 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Sockets;
 using UnityEngine;
 
 public class HumanVisualController : MonoBehaviour
 {
     public Animator anim;
+
+    [Header("Ragdoll")] 
+    public List<Transform> animatedBones;
+    public List<ConfigurableJoint> joints;
+    List<Quaternion> initRotations = new List<Quaternion>();
+    private void Start()
+    {
+        for (int i = 0; i < joints.Count; i++)
+        {
+            initRotations.Add(animatedBones[i].localRotation);
+        }
+    }
+
     public void SetMovementVelocity(Vector3 velocity)
     {
         float velocityX = Vector3.Dot(velocity.normalized, transform.right);
@@ -13,5 +27,18 @@ public class HumanVisualController : MonoBehaviour
         
         anim.SetFloat("VelocityX", velocityX, 0.1f, Time.deltaTime);
         anim.SetFloat("VelocityZ", velocityZ, 0.1f, Time.deltaTime);
+    }
+
+    private void FixedUpdate()
+    {
+        for (int i = 0; i < joints.Count; i++)
+        {
+            joints[i].targetRotation = CopyRotation(i);
+        }
+    }
+
+    Quaternion CopyRotation(int index)
+    {
+        return Quaternion.Inverse(animatedBones[index].localRotation) * initRotations[index];
     }
 }
