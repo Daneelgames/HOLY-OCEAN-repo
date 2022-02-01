712f49caf7499e65357f3dcc87c9728274c02f5d
MrPink <rubberandrobber@gmail.com>
2022-02-01 12:03:43 +0300

    proc buildings pt1

--- PartyFpsTactics/Assets/LevelGenerator.cs
+++ PartyFpsTactics/Assets/LevelGenerator.cs
@@ -8,110 +8,144 @@ using Random = UnityEngine.Random;
 public class LevelGenerator : MonoBehaviour
 {
     public GameObject tilePrefab;
-    public List<Room> spawnedRooms = new List<Room>();
+    public List<Level> spawnedLevels = new List<Level>();
 
-    public Transform levelFolder;
+    public Transform generatedBuildingFolder;
+    [Header("SETTINGS")]
+    public List<int> levelsHeights = new List<int>();
+    [Range(1,10)]
+    public Vector2Int roomsPerLevelMinMax = new Vector2Int(1, 10);
+    public bool randomLevelRotation = false;
+
     [Header("SCALE IS SCALED BY 2 IN CODE")]
-    public Vector2Int roomsScaleMinMaxX = new Vector2Int(3, 10);
+    public Vector2Int levelsScaleMinMaxX = new Vector2Int(3, 10);
     public Vector2Int roomsScaleMinMaxY = new Vector2Int(2, 6);
     public Vector2Int roomsScaleMinMaxZ = new Vector2Int(3, 10);
-    public Vector2Int roomsPositionsMinMaxX = new Vector2Int(-100, 100);
-    public Vector2Int roomsPositionsMinMaxY = new Vector2Int(0, 100);
-    public Vector2Int roomsPositionsMinMaxZ = new Vector2Int(-100, 100);
     
-    public int roomsAmount = 10;
     IEnumerator Start()
     {
-        for (int i = 0; i < roomsAmount; i++)
+        if (generatedBuildingFolder == null)
         {
-            yield return StartCoroutine(SpawnNewRoom(i));
-            yield return null;
+            generatedBuildingFolder = new GameObject("GeneratedBuilding").transform;
+            generatedBuildingFolder.position = Vector3.zero;
         }
-
-        yield return StartCoroutine(ConnectRooms());
         
+        if (levelsHeights.Count == 0) // default 5 floors building
+        {
+            levelsHeights = new List<int>();
+            for (int i = 0; i < 5; i++)
+            {
+                levelsHeights.Add(5);
+            }
+        }
+
+        for (int i = 0; i < levelsHeights.Count; i++)
+        {
+            Debug.Log("Spawn New Level " + i);
+            yield return StartCoroutine(SpawnNewLevel(i));
+        }
+        
+        yield return StartCoroutine(ConnectRooms());
     }
 
-    IEnumerator SpawnNewRoom(int roomIndex)
+    
+    IEnumerator SpawnNewLevel(int levelIndex)
     {
-        while (true)
+        float levelY = 0;
+        
+        if (levelIndex > 0)
         {
-            float roomY = roomsPositionsMinMaxY.x;
-            
-            if (roomIndex > 0)
-            {
-                roomY += roomIndex * 5;
-            }
-            
-            Vector3 roomPosition = new Vector3(Random.Range(roomsPositionsMinMaxX.x, roomsPositionsMinMaxX.y),
-                roomY, Random.Range(roomsPositionsMinMaxZ.x, roomsPositionsMinMaxZ.y));
-            
-            Vector3Int roomSize = new Vector3Int(Random.Range(roomsScaleMinMaxX.x, roomsScaleMinMaxX.y) * 2,
-                Random.Range(roomsScaleMinMaxY.x, roomsScaleMinMaxY.y) * 2,Random.Range(roomsScaleMinMaxZ.x, roomsScaleMinMaxZ.y) * 2);
-            Quaternion roomRotation = Quaternion.Euler(0, Random.Range(0,360), 0);
+            levelY = levelIndex * 5;
+        }
+        
+        Vector3 levelPosition = new Vector3(0, levelY, 0);
+        
+        Vector3Int levelSize = new Vector3Int(Random.Range(levelsScaleMinMaxX.x, levelsScaleMinMaxX.y) * 2,
+            levelsHeights[levelIndex],Random.Range(roomsScaleMinMaxZ.x, roomsScaleMinMaxZ.y) * 2);
+        
+        Quaternion levelRotation = Quaternion.identity;
+        if (randomLevelRotation)
+            levelRotation = Quaternion.Euler(0, Random.Range(0,360), 0);
 
-            if (!Physics.CheckBox(roomPosition, new Vector3(roomSize.x, roomSize.y, roomSize.z), roomRotation, 1 << 6))
+        yield return StartCoroutine(SpawnLevelTiles(levelIndex, levelPosition, levelSize, levelRotation));
+        if (levelIndex == 0)
+        {
+            PlayerMovement.Instance.rb.MovePosition(levelPosition);
+        }
+        yield break;
+        
+        if (!Physics.CheckBox(levelPosition, new Vector3(levelSize.x, levelSize.y, levelSize.z), levelRotation, 1 << 6))
+        {
+            yield return StartCoroutine(SpawnLevelTiles(levelIndex, levelPosition, levelSize, levelRotation));
+            if (levelIndex == 0)
             {
-                yield return StartCoroutine(SpawnTilesInRoom(roomPosition, roomSize, roomRotation));
-                if (roomIndex == 0)
-                {
-                    PlayerMovement.Instance.rb.MovePosition(roomPosition);
-                }
-                yield break;
+                PlayerMovement.Instance.rb.MovePosition(levelPosition);
             }
-            yield return null;
+            yield break;
         }
     }
 
-    IEnumerator SpawnTilesInRoom(Vector3 pos, Vector3Int size, Quaternion rot)
+    IEnumerator SpawnLevelTiles(int index, Vector3 pos, Vector3Int size, Quaternion rot)
     {
-        Room newRoom = new Room();
-        newRoom.position = pos;
-        newRoom.size = size;
-        newRoom.rotation = rot;
+        Level newLevel = new Level();
+        newLevel.position = pos;
+        newLevel.size = size;
+        newLevel.rotation = rot;
 
-        GameObject newGameObject = new GameObject("Room");
-        newGameObject.transform.parent = levelFolder;
-        newRoom.spawnedTransform = newGameObject.transform;
+        GameObject newGameObject = new GameObject("Level " + index);
+        newGameObject.transform.parent = generatedBuildingFolder;
+        newLevel.spawnedTransform = newGameObject.transform;
         newGameObject.transform.position = pos;
         newGameObject.transform.rotation = rot;
         
-        spawnedRooms.Add(newRoom);
         for (int x = 0; x < size.x; x++)
         {
             for (int z = 0; z < size.z; z++)
             {
-                var newTile = Instantiate(tilePrefab, newRoom.spawnedTransform);
+                var newTile = Instantiate(tilePrefab, newLevel.spawnedTransform);
                 newTile.transform.localRotation = Quaternion.identity;
                 newTile.transform.localPosition = new Vector3(x - size.x / 2, 0, z - size.z/2);
-                newRoom.tiles.Add(newTile);
+                newLevel.tiles.Add(newTile);
+
+                if (x == 0 || x == size.x - 1 || z == 0 || z == size.z - 1) // SPAWN OUTSIDE WALLS
+                {
+                    for (int y = 1; y < levelsHeights[index]; y++)
+                    {
+                        var newWallTile = Instantiate(tilePrefab, newLevel.spawnedTransform);
+                        newWallTile.transform.localRotation = Quaternion.identity;
+                        newWallTile.transform.position = newTile.transform.position + Vector3.up * y;
+                        newLevel.tiles.Add(newWallTile);
+                    }
+                }
             }
             yield return null;   
         }
+        
+        spawnedLevels.Add(newLevel);
     }
 
     IEnumerator ConnectRooms()
     {
-        for (int i = 0; i < spawnedRooms.Count - 1; i++)
+        for (int i = 0; i < spawnedLevels.Count - 1; i++)
         {
-            Room roomFrom = spawnedRooms[i];
-            Room roomTo = spawnedRooms[i + 1];
+            Level levelFrom = spawnedLevels[i];
+            Level levelTo = spawnedLevels[i + 1];
 
-            Transform roomFromClosestTile = roomFrom.tiles[0].transform;
-            Transform roomToClosestTile = roomTo.tiles[0].transform;
+            Transform roomFromClosestTile = levelFrom.tiles[0].transform;
+            Transform roomToClosestTile = levelTo.tiles[0].transform;
             
             float distance = 10000;
             float newDistance = 0;
             
-            for (int j = 0; j < roomFrom.tiles.Count; j++)
+            for (int j = 0; j < levelFrom.tiles.Count; j++)
             {
-                for (int k = 0; k < roomTo.tiles.Count; k++)
+                for (int k = 0; k < levelTo.tiles.Count; k++)
                 {
-                    if (j != 0 && k != 0 && j != roomFrom.tiles.Count-1 && k != roomTo.tiles.Count-1)
+                    if (j != 0 && k != 0 && j != levelFrom.tiles.Count-1 && k != levelTo.tiles.Count-1)
                         continue;
                     
-                    var tile1 = roomFrom.tiles[j];
-                    var tile2 = roomTo.tiles[k];
+                    var tile1 = levelFrom.tiles[j];
+                    var tile2 = levelTo.tiles[k];
                     newDistance = Vector3.Distance(tile1.transform.position, tile2.transform.position);
                     if (newDistance < distance)
                     {
@@ -128,7 +162,7 @@ public class LevelGenerator : MonoBehaviour
                 Quaternion rot = Quaternion.Lerp(roomFromClosestTile.rotation, roomToClosestTile.rotation, j/bridgeTilesAmount);
                 var newTile = Instantiate(tilePrefab, 
                     roomFromClosestTile.position + (roomToClosestTile.position - roomFromClosestTile.position).normalized * j, rot);
-                newTile.transform.parent = levelFolder;
+                newTile.transform.parent = generatedBuildingFolder;
                 yield return null;
             }
             yield return null;
@@ -137,11 +171,11 @@ public class LevelGenerator : MonoBehaviour
 }
 
 [Serializable]
-public class Room
+public class Level
 {
     public List<GameObject> tiles = new List<GameObject>();
     public Transform spawnedTransform;
     public Vector3 position;
-    public  Vector3Int size;
-    public  Quaternion rotation;
+    public Vector3Int size;
+    public Quaternion rotation;
 }
