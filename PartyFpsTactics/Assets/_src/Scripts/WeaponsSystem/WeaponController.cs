7d6cbf753a0d8c931f8fdefdde515d2496d50e55
MrPink <rubberandrobber@gmail.com>
2022-03-25 22:26:27 +0300

    dollars

--- PartyFpsTactics/Assets/_src/Scripts/WeaponsSystem/WeaponController.cs
+++ PartyFpsTactics/Assets/_src/Scripts/WeaponsSystem/WeaponController.cs
@@ -5,17 +5,21 @@ using MrPink.Health;
 using MrPink.PlayerSystem;
 using Unity.VisualScripting;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class WeaponController : MonoBehaviour
 {
     public Transform shotHolder;
     public ProjectileController projectilePrefab;
+    public float delay = 0;
     public float cooldown = 1;
     bool onCooldown = false;
 
     public Transform raycastStartPoint;
     public Transform raycastEndPoint;
 
+    public AudioSource attackSignalAu;
+    
     private Quaternion _initLocalRotation;
     public Quaternion InitLocalRotation
     {
@@ -35,23 +39,28 @@ public class WeaponController : MonoBehaviour
     
     public void Shot(HealthController ownerHc)
     {
-        Shot(shotHolder.forward, ownerHc);
+        StartCoroutine(Shot(shotHolder.forward, ownerHc));
     }
 
-    public void Shot(Vector3 direction, HealthController ownerHc)
+    public IEnumerator Shot(Vector3 direction, HealthController ownerHc)
     {
+        OnCooldown = true;
+        if (attackSignalAu)
+        {
+            attackSignalAu.pitch = Random.Range(0.75f, 1.25f);
+            attackSignalAu.Play();
+        }
+        yield return new WaitForSeconds(delay);
+        
+        if (ownerHc.health <= 0)
+            yield break;
+        
         var newProjectile = Instantiate(projectilePrefab, shotHolder.position, Quaternion.LookRotation(direction));
         ScoringActionType action = ScoringActionType.NULL;
         if (ownerHc == Player.Health)
             action = Player.Movement.GetCurrentScoringAction();
         
         newProjectile.Init(ownerHc, action);
-        StartCoroutine(Cooldown());
-    }
-
-    IEnumerator Cooldown()
-    {
-        OnCooldown = true;
         yield return new WaitForSeconds(cooldown);
         OnCooldown = false;
     }
