e75a989905a560fe166fdb0af7626c6686dcb08d
MrPink <rubberandrobber@gmail.com>
2022-04-03 18:22:58 +0300

    random npc clothes pipeline pt0

--- PartyFpsTactics/Assets/ActivateRandomObject.cs
+++ PartyFpsTactics/Assets/ActivateRandomObject.cs
@@ -1,23 +1,38 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class ActivateRandomObject : MonoBehaviour
 {
-    public List<GameObject> objectsToActivate;
+    public List<ListOfGameObjects> objectsToActivate;
 
     void Start()
     {
         int targetIndex = Random.Range(0, objectsToActivate.Count);
 
-        objectsToActivate[targetIndex].SetActive(true);
+        for (int j = 0; j < objectsToActivate[targetIndex].GameObjects.Count; j++)
+        {
+            objectsToActivate[targetIndex].GameObjects[j].SetActive(true);
+        }
         
         for (int i = objectsToActivate.Count - 1; i >= 0; i--)
         {
             if (targetIndex != i)
             {
-                Destroy(objectsToActivate[i]);
+                
+                for (int j = 0; j < objectsToActivate[i].GameObjects.Count; j++)
+                {
+                    Destroy(objectsToActivate[i].GameObjects[j]);
+                }
             }    
         }
     }
+}
+
+[Serializable]
+public class ListOfGameObjects
+{
+    public List<GameObject> GameObjects;
 }
